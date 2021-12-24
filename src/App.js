9f75c4785401560c09e806ec0bd0f66f3ace7a91
Shane Sutro <shane@shanesutro.dev>
2021-12-24 11:25:59 -0700

    Add interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Box, AppBar, Typography, Card, Divider } from '@mui/material';
+import dayjs from 'dayjs';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://xd610oobw6.execute-api.us-east-1.amazonaws.com/prod/all')
+      .then((response) => response.data.data.Items)
+      .then((items) => {
+        console.log(items);
+        setArticles(items);
+        setError('');
+      })
+      .catch((err) => setError(err));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>VB News Stage</h1>
-    </div>
+    <Box className="App">
+      <AppBar position="sticky">
+        <Typography variant="h3">VB News</Typography>
+      </AppBar>
+      <Typography sx={{ mt: '1rem' }} variant="h4">
+        Recently Posted Articles
+      </Typography>
+      <Box sx={{ display: 'grid', gap: '1rem', padding: '1rem' }}>
+        {articles.map((article) => (
+          <Card
+            elevation={8}
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: '.8rem 1fr',
+              minHeight: '5rem',
+              gap: '1rem',
+            }}
+            onClick={() => window.open(article.link, '_blank')}
+          >
+            <Box sx={{ height: '100%', width: '100%', backgroundColor: 'red' }} />
+            <Box sx={{ paddingRight: '1rem', paddingY: '1rem' }}>
+              <Typography sx={{ fontWeight: 700 }} align="left">
+                {article.headline}
+              </Typography>
+              <Divider sx={{ marginY: '.5rem' }} />
+              <Typography sx={{ fontStyle: 'italic' }} align="left" variant="subtitle2">{`${
+                article.publisher
+              } on ${dayjs(article.addes).format('MMMM DD, YYYY')}`}</Typography>
+            </Box>
+          </Card>
+        ))}
+      </Box>
+    </Box>
   );
 }
 
